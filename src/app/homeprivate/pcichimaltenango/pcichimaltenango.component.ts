8a0710e65fa5519f51ee33adff946fd838b0507c
dipafront <informaticadipafront@gmail.com>
2023-09-27 08:57:16 -0600

    27/09/2023

--- src/app/homeprivate/pcichimaltenango/pcichimaltenango.component.ts
+++ src/app/homeprivate/pcichimaltenango/pcichimaltenango.component.ts
@@ -6,6 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./pcichimaltenango.component.css']
 })
 export class PcichimaltenangoComponent {
-  pdfSrc = "https://vadimdez.github.io/ng2-pdf-viewer/assets/pdf-test.pdf";
 
 }
